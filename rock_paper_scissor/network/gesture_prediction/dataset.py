10bb358aaa07d8ed1c6c181cdb68ba3963253cdc
Xingyu Fu <Xingyu.Fu@anu.edu.au>
2022-11-09 20:11:37 +1100

    fix: dataset only takes 100 data

--- rock_paper_scissor/network/gesture_prediction/dataset.py
+++ rock_paper_scissor/network/gesture_prediction/dataset.py
@@ -22,11 +22,11 @@ def read_data():
     scissor_data_path = "rock_paper_scissor/network/gesture_prediction/data/scissor.npy"
 
     paper_data = np.load(paper_data_path).astype(np.float32)
-    paper_data = paper_data[:100, :15, :]
+    paper_data = paper_data[:, :15, :]
     rock_data = np.load(rock_data_path).astype(np.float32)
-    rock_data = rock_data[:100, :15, :]
+    rock_data = rock_data[:, :15, :]
     scissor_data = np.load(scissor_data_path).astype(np.float32)
-    scissor_data = scissor_data[:100, :15, :]
+    scissor_data = scissor_data[:, :15, :]
     # concatenate data
     data = np.concatenate((paper_data, rock_data, scissor_data), axis=0)
     labels = np.concatenate((np.repeat(0, len(paper_data)), np.repeat(1, len(rock_data)), np.repeat(2, len(scissor_data))))
